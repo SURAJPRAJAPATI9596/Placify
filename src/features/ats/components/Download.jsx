6010b6d2445b44304bdcf22f8e032619bddad622
SURAJPRAJAPATI9596 <SURAJPRAJAPATIJOB@GMAIL.COM>
2026-06-25 17:05:00 +0530

    fixed problem in download section

--- src/features/ats/components/Download.jsx
+++ src/features/ats/components/Download.jsx
@@ -2,15 +2,12 @@ import React from "react";
 
 const Download = ({ dark }) => {
   return (
-    <div
-      className={`mt-10 p-8 rounded-xl text-center ${
-        dark ? "bg-gray-900" : "bg-white"
-      }    bg-(--bg-primary)
-      text-(--text-primary)`}
-    >
+    <div className="mt-10 p-8 rounded-xl text-center  bg-(--bg-primary) text-(--text-primary)">
       <h2 className="text-3xl font-bold">🟢 ATS Friendly</h2>
 
-      <p className="mt-3">Your resume can successfully pass ATS systems.</p>
+      <p className="mt-3 bg-(--bg-primary) text-(--text-primary)">
+        Your resume can successfully pass ATS systems.
+      </p>
 
       <button
         className="
